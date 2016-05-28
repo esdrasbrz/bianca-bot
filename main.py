ab42092a7882d559d234b3d2ed70d850f42273ce
Esdras Rodrigues do Carmo <esdrasbrz@gmail.com>
2016-05-28 12:24:29 -0300

    Implementado a conexão

---
+++ main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Arquivo principal do BOT, que checa as postagens sobre medicina e posta automaticamente
+no perfil.
+
+@author Esdras R. Carmo
+"""
+
+import twitter
+
+## Constantes do APP
+CONSUMER_KEY = "iG3uFTZS7iNqGVlroey3cV5os"
+CONSUMER_SECRET = "bUZmQxZnaZLfPKeUIRC7EsfpK6S06RUwFHtGqWaaWnduXsC55C"
+ACCESS_TOKEN = "736567186169430016-AoxkDtl5irWfSp6XryCDDdKmziOiRsI"
+ACCESS_TOKEN_SECRET = "v1X0bORIMR9yYDdpTNUgJMyJuEIHLJA3fnQZy77TKgWjK"
+
+
+## Abre a conexao
+print("Conectando-se")
+api = twitter.Api(consumer_key        = CONSUMER_KEY,    \
+                  consumer_secret     = CONSUMER_SECRET, \
+                  access_token_key    = ACCESS_TOKEN,    \
+                  access_token_secret = ACCESS_TOKEN_SECRET)
+print("Conectado com sucesso!")
